07c6e234e61b01133c1d261dfde3d1e91b1d903a
aaxwaz <wangweimin777@gmail.com>
2014-12-17 08:57:34 +0800

    Update MyToy.py

--- MyToy.py
+++ MyToy.py
@@ -1,5 +1,5 @@
 class MyToy: 
-    #__slots__ = ['toyId', 'arrivalTime', 'duration']
+    __slots__ = ['toyId', 'arrivalTime', 'duration']
     def __init__(self, toyId, arrivalTime, duration):
         self.toyId = toyId
         self.arrivalTime = arrivalTime
