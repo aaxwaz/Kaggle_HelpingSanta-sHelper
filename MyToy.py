f877ec3d04459ec04aa3ce417203675c85f81946
aaxwaz <wangweimin777@gmail.com>
2014-12-17 08:57:19 +0800

    Create MyToy.py

---
+++ MyToy.py
@@ -0,0 +1,6 @@
+class MyToy: 
+    #__slots__ = ['toyId', 'arrivalTime', 'duration']
+    def __init__(self, toyId, arrivalTime, duration):
+        self.toyId = toyId
+        self.arrivalTime = arrivalTime
+        self.duration = duration
