010fe19912cdbb6d76cdcc37a493410a985532a5
aaxwaz <wangweimin777@gmail.com>
2014-12-19 10:55:56 +0800

    Create MyElf.py

---
+++ MyElf.py
@@ -0,0 +1,6 @@
+class MyElf: 
+    __slots__ = ['avaiTime', 'id', 'productivity']
+    def __init__(self, avaiTime, id, productivity):
+        self.avaiTime = avaiTime
+        self.id = id
+        self.productivity = productivity
