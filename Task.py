9b26f9f63ba0a0aa447dff1916264a77acbae533
aaxwaz <wangweimin777@gmail.com>
2014-12-23 10:07:46 +0800

    Update Task.py

--- Task.py
+++ Task.py
@@ -11,7 +11,7 @@ class Task:
         actualWorkingMinutes = int(math.ceil(thisToyDuration/thisElfProd)) # int 
         actualStartTime = thisElfAvaiTime # datetime  int
         actualCompleteTime = actualStartTime + timedelta(minutes = actualWorkingMinutes)# datetime int
-        if actualCompleteTime <= endOfToday:      # can fit in
+        if actualCompleteTime < endOfToday:      # can fit in
             n = actualWorkingMinutes
             updatedProductivity = thisElfProd * (1.02**(n/60.0))
             updatedProductivity  = 4.0 if updatedProductivity > 4.0 else updatedProductivity
@@ -20,14 +20,15 @@ class Task:
             # return values: 
             result = [updatedProductivity, nextAvaiTime, actualWorkingMinutes]
             return result
-        else:           #long working hours 
+        else:                                     # cannot fit in
             numberOfDaysSpent = actualWorkingMinutes/float(24*60) # float
             numberOfWholeDays = math.floor(numberOfDaysSpent) # float
             numberOfFractionDay = numberOfDaysSpent - numberOfWholeDays # float
             workingMinutesOfToday = (endOfToday - thisElfAvaiTime).seconds/60.0 #float
             
-            if actualCompleteTime.hour >= 9 and actualCompleteTime.hour < 19:  # situation 1: after next day 9am
-                m = (actualCompleteTime.day - today.day) * 14.0 * 60.0 # float in minutes
+            if actualCompleteTime.hour >= 9 and actualCompleteTime.hour < 19:  # situation 1: after 9am
+                startOfToday = datetime(thisElfAvaiTime.year, thisElfAvaiTime.month, thisElfAvaiTime.day, 9,0)
+                m = (actualCompleteTime - startOfToday).days * 14.0 * 60.0 # float in minutes
                 n = actualWorkingMinutes - m                           # float in minutes
                 updatedProductivity = thisElfProd * (1.02**(n/60.0)) * (0.9**(m/60.0))
                 updatedProductivity = 0.25 if updatedProductivity < 0.25 else updatedProductivity
@@ -44,7 +45,8 @@ class Task:
             wholeRestDaysNeeded = math.floor(restDaysNeeded) # float
             remainingRestMinutes = timedelta(minutes=(m - wholeRestDaysNeeded * 60 * 10)) #float in minutes (whole number)
             endOfActualCompleteDay = datetime(actualCompleteTime.year, actualCompleteTime.month, actualCompleteTime.day, 19,0)
-            if actualCompleteTime + remainingRestMinutes <= endOfActualCompleteDay:
+            
+            if actualCompleteTime + remainingRestMinutes < endOfActualCompleteDay:
                 if actualCompleteTime.hour >= 9: 
                     nextAvaiTime = actualCompleteTime + remainingRestMinutes + timedelta(days = wholeRestDaysNeeded)
                 else:
@@ -58,4 +60,3 @@ class Task:
             
             # return values: 
             return updatedProductivity, nextAvaiTime, actualWorkingMinutes
-            
